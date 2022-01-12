c061ddf5b40f766f2764b2c8b650a5c7cdb432c2
Michal Dengusiak <tassimulation@gmail.com>
2022-01-12 20:30:59 +0000

    add icon

--- Grasshopper/SAM.Analytical.Grasshopper.SolarCalculator/Properties/Resources.Designer.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.SolarCalculator/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace SAM.Analytical.Grasshopper.SolarCalculator.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SAM_SolarCalculator {
+            get {
+                object obj = ResourceManager.GetObject("SAM_SolarCalculator", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- Grasshopper/SAM.Core.Grasshopper.SolarCalculator/Properties/Resources.Designer.cs
+++ Grasshopper/SAM.Core.Grasshopper.SolarCalculator/Properties/Resources.Designer.cs
@@ -129,5 +129,15 @@ namespace SAM.Core.Grasshopper.SolarCalculator.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SAM_SolarCalculator {
+            get {
+                object obj = ResourceManager.GetObject("SAM_SolarCalculator", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
