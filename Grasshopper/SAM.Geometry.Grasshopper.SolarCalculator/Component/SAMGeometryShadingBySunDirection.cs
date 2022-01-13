e06e5cd7366e7f2bad574ce392841a963f97b546
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-13 17:28:13 +0000

    Shading Calculations Added

---
+++ Grasshopper/SAM.Geometry.Grasshopper.SolarCalculator/Component/SAMGeometryShadingBySunDirection.cs
@@ -0,0 +1,124 @@
+﻿using Grasshopper.Kernel;
+using SAM.Geometry.Grasshopper.SolarCalculator.Properties;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.SolarCalculator;
+using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel.Types;
+
+namespace SAM.Geometry.Grasshopper.SolarCalculator
+{
+    public class SAMGeometryShadingBySunDirection : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("46f6432c-78a3-49d2-ad5f-8162ee5374ff");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_SolarCalculator;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMGeometryShadingBySunDirection()
+          : base("SAMGeometry.ShadingBySunDirection", "SAMGeometry.ShadingBySunDirection",
+              "Gets Shading Geometry for given Geometry and sun direction",
+              "SAM WIP", "Solar")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooSAMGeometryParam() { Name = "_geometries", NickName = "_geometries", Description = "SAM Geometry", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Vector() { Name = "_sunDirection", NickName = "_sunDirection", Description = "Sun Direction", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooSAMGeometryParam() { Name = "shadings", NickName = "shadings", Description = "Shading Geometries", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+
+            index = Params.IndexOfInputParam("_sunDirection");
+            global::Rhino.Geometry.Vector3d vector3d = global::Rhino.Geometry.Vector3d.Unset;
+            if (index == -1 || !dataAccess.GetData(index, ref vector3d) || vector3d == global::Rhino.Geometry.Vector3d.Unset)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("_geometries");
+            List<GH_ObjectWrapper> objectWrappers = new List<GH_ObjectWrapper>();
+            if (index == -1 || !dataAccess.GetDataList(index, objectWrappers) || objectWrappers== null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<SolarFace> solarFaces = new List<SolarFace>();
+            foreach (GH_ObjectWrapper objectWrapper in objectWrappers)
+            {
+                if (!Query.TryGetSAMGeometries(objectWrapper, out List<ISAMGeometry> sAMGeometries) || sAMGeometries == null)
+                {
+                    continue;
+                }
+
+                foreach(ISAMGeometry sAMGeometry in sAMGeometries)
+                {
+                    if(sAMGeometry is Spatial.Face3D)
+                    {
+                        solarFaces.Add(new SolarFace(Guid.NewGuid(), sAMGeometry as Spatial.Face3D));
+                    }
+                    else if(sAMGeometry is Spatial.IFace3DObject)
+                    {
+                        Core.SAMObject sAMObject = sAMGeometry as Core.SAMObject;
+
+                        solarFaces.Add(new SolarFace(sAMObject == null ? Guid.NewGuid() : sAMObject.Guid, ((Spatial.IFace3DObject)sAMGeometry).Face3D));
+                    }
+                }
+            }
+
+            List<SolarFace> solarFaces_Result = Geometry.SolarCalculator.Query.Shadings(solarFaces, Rhino.Convert.ToSAM(vector3d));
+
+            index = Params.IndexOfOutputParam("shadings");
+            if (index != -1)
+                dataAccess.SetDataList(index, solarFaces_Result?.ConvertAll(x => x?.Face3D));
+        }
+    }
+}
---
+++ SAM_SolarCalculator/SAM.Geometry.SolarCalculator/Query/Shadings.cs
@@ -0,0 +1,186 @@
+﻿using SAM.Geometry.Spatial;
+using System;
+using SAM.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Geometry.SolarCalculator
+{
+    public static partial class Query
+    {
+        public static List<SolarFace> Shadings(this IEnumerable<SolarFace> solarFaces, Vector3D sunDirection, double tolerance_Area = Tolerance.MacroDistance, double tolerance_Snap = Tolerance.MacroDistance, double tolerance_Distance = Tolerance.Distance)
+        {
+            if(solarFaces == null || sunDirection == null || !sunDirection.IsValid())
+            {
+                return null;
+            }
+
+            List<SolarFace> result = new List<SolarFace>();
+
+            if(solarFaces.Count() < 2)
+            {
+                return result;
+            }
+
+            BoundingBox3D boundingBox3D = Spatial.Create.BoundingBox3D(solarFaces);
+            if(boundingBox3D == null || !boundingBox3D.IsValid())
+            {
+                return result;
+            }
+
+            Vector3D vector3D = new Vector3D(sunDirection).Unit * boundingBox3D.Min.Distance(boundingBox3D.Max);
+            Point3D point3D = boundingBox3D.GetCentroid().GetMoved(vector3D.GetNegated()) as Point3D;
+
+            Plane plane = new Plane(point3D, vector3D.Unit);
+
+            List<SolarFace> solarFaces_Filtered = new List<SolarFace>();
+            List<Planar.Segment2D> segment2Ds = new List<Planar.Segment2D>();
+            foreach(SolarFace solarFace in solarFaces)
+            {
+                Face3D face3D = solarFace?.Face3D;
+                if(face3D == null)
+                {
+                    continue;
+                }
+
+                Face3D face3D_Project = plane.Project(face3D, vector3D, tolerance_Distance);
+                if(face3D_Project == null || !face3D_Project.IsValid())
+                {
+                    result.Add(solarFace);
+                    continue;
+                }
+
+                Planar.Face2D face2D = plane.Convert(face3D_Project);
+                if(face2D == null || face2D.GetArea() < tolerance_Area)
+                {
+                    result.Add(solarFace);
+                    continue;
+                }
+
+                Planar.ISegmentable2D segmentable2D = face2D.ExternalEdge2D as Planar.ISegmentable2D;
+                if(segmentable2D != null)
+                {
+                    List<Planar.Segment2D> segment2Ds_Temp = segmentable2D.GetSegments();
+                    if(segment2Ds_Temp != null)
+                    {
+                        segment2Ds.AddRange(segment2Ds_Temp);
+                    }
+                }
+
+                List<Planar.IClosed2D> closed2Ds = face2D.InternalEdge2Ds;
+                if(closed2Ds != null)
+                {
+                    foreach(Planar.IClosed2D closed2D in closed2Ds)
+                    {
+                        List<Planar.Segment2D> segment2Ds_Temp = (closed2D as Planar.ISegmentable2D)?.GetSegments();
+                        if (segment2Ds_Temp != null)
+                        {
+                            segment2Ds.AddRange(segment2Ds_Temp);
+                        }
+                    }
+                }
+
+                solarFaces_Filtered.Add(solarFace);
+            }
+
+            if(segment2Ds == null || solarFaces_Filtered == null)
+            {
+                return result;
+            }
+
+            segment2Ds = Planar.Query.Split(segment2Ds, tolerance_Distance);
+
+            segment2Ds = Planar.Query.Snap(segment2Ds, true, tolerance_Snap);
+
+            List<Planar.Polygon2D> polygon2Ds = Planar.Create.Polygon2Ds(segment2Ds, tolerance_Distance);
+            if (polygon2Ds == null)
+            {
+                return result;
+            }
+
+            Vector3D vector3D_Ray =  2 * vector3D;
+
+            List<Tuple<Planar.Polygon2D, List<SolarFace>>> tuples = new List<Tuple<Planar.Polygon2D, List<SolarFace>>>();
+            foreach(Planar.Polygon2D polygon2D in polygon2Ds)
+            {
+                Planar.Point2D point2D = polygon2D?.GetInternalPoint2D(tolerance_Distance);
+                if(point2D == null)
+                {
+                    continue;
+                }
+
+                Point3D point3D_Start = plane.Convert(point2D);
+                Point3D point3D_End = point3D_Start.GetMoved(vector3D_Ray) as Point3D;
+
+                Segment3D segment3D = new Segment3D(point3D_Start, point3D_End);
+
+                List<SolarFace> solarFaces_Polygon2D = solarFaces_Filtered.FindAll(x => x.GetBoundingBox().InRange(segment3D, tolerance_Distance));
+
+                Dictionary<SolarFace, Point3D> dictionary_Intersection = Spatial.Query.IntersectionDictionary(segment3D, solarFaces_Polygon2D, true, tolerance_Distance);
+                if(dictionary_Intersection == null || dictionary_Intersection.Count < 2)
+                {
+                    continue;
+                }
+
+                List<SolarFace> solarFaces_Temp = new List<SolarFace>(dictionary_Intersection.Keys);
+                solarFaces_Temp.RemoveAt(0);
+
+                tuples.Add(new Tuple<Planar.Polygon2D, List<SolarFace>>(polygon2D, solarFaces_Temp));
+            }
+
+            if(tuples == null || tuples.Count == 0)
+            {
+                return result;
+            }
+
+            foreach(SolarFace solarFace in solarFaces_Filtered)
+            {
+                List<Tuple<Planar.Polygon2D, List<SolarFace>>> tuples_SolarFace = tuples.FindAll(x => x.Item2.Contains(solarFace));
+                if(tuples_SolarFace == null || tuples_SolarFace.Count == 0)
+                {
+                    continue;
+                }
+
+                List<Planar.Polygon2D> polygon2Ds_SolarFace = Planar.Query.Union(tuples_SolarFace.ConvertAll(x => x.Item1), tolerance_Distance);
+                if(polygon2Ds_SolarFace == null || polygon2Ds_SolarFace.Count == 0)
+                {
+                    continue;
+                }
+
+                Face3D face3D_SolarFace = solarFace.Face3D;
+                Plane plane_SolarFace = face3D_SolarFace.GetPlane();
+                Planar.Face2D face2D = plane_SolarFace.Convert(face3D_SolarFace);
+                
+                foreach(Planar.Polygon2D polygon2D_SolarFace in polygon2Ds_SolarFace)
+                {
+                    Polygon3D polygon3D_Shade = plane.Convert(polygon2D_SolarFace);
+
+                    polygon3D_Shade = plane_SolarFace.Project(polygon3D_Shade, vector3D, tolerance_Distance);
+                    if(polygon3D_Shade == null)
+                    {
+                        continue;
+                    }
+
+                    Planar.Polygon2D polygon2D_Shade = plane_SolarFace.Convert(polygon3D_Shade);
+                    if(polygon2D_Shade == null)
+                    {
+                        continue;
+                    }
+
+                    List<Planar.Face2D> face2Ds_Shade = Planar.Query.Intersection(face2D, new Planar.Face2D(polygon2D_Shade));
+                    if(face2Ds_Shade == null || face2Ds_Shade.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    foreach(Planar.Face2D face2D_Shade in face2Ds_Shade)
+                    {
+                        result.Add(new SolarFace(solarFace.Guid, plane_SolarFace.Convert(face2D_Shade)));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
